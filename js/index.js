52d27eb9493c1c91f295a46ed63e5a57f085a7b6
jgiron730 <badum7@gmail.com>
2020-04-07 22:43:52 -0600

    Edit finished

--- js/index.js
+++ js/index.js
@@ -1,8 +1,33 @@
 var miForm = document.getElementById('formulario')
+var miEdit = document.getElementById('editando')
 var lista = document.getElementById('lista')
+var cancelar = document.getElementById('cancel')
 
 
 var initialState = { valor: 0, lista: [] };
+
+var llave=0
+
+const mostrar =(s,valor,k)=>{
+    llave=k;
+    miEdit[0].value=valor
+
+    console.log(miEdit[0])
+
+    if(s){
+        miForm.style.display = 'block'
+        miEdit.style.display = 'none'
+    }else{
+        miForm.style.display = 'none'
+        miEdit.style.display = 'block'
+    }
+}
+
+cancelar.addEventListener('click', function(){
+    mostrar(true)
+})
+
+mostrar(true)
 
 const add = (i, t) => {
     return {
@@ -18,6 +43,13 @@ const borrar = (i) => {
         tarea: i
     }
 }
+const editar = (i,t) => {
+    return {
+        type: 'EDIT',
+        id: i,
+        tarea: t
+    }
+}
 
 const reductor = (estado = initialState, accion) => {
     switch (accion.type) {
@@ -25,6 +57,9 @@ const reductor = (estado = initialState, accion) => {
             return { ...estado, lista: [...estado.lista, { id: accion.id, tarea: accion.tarea }] }
         case 'DELETE':
             return { ...estado, lista: estado.lista.filter((x, k) => k != accion.id) }
+        case 'EDIT':
+            return { ...estado, lista: estado.lista.map((x, k) => k == accion.id ? {...x, tarea: accion.tarea}  : x ) }
+
         default:
             return estado
     }
@@ -45,6 +80,17 @@ miForm.addEventListener('submit', function (e) {
         alert('Dont be lazy! Add a task.')
     }
 })
+miEdit.addEventListener('submit', function (e) {
+    console.log(e.target[0].value)
+    e.preventDefault()
+    if (e.target[0].value !== "") {
+
+        tienda.dispatch(editar(llave, e.target[0].value))
+        
+    } else {
+        alert('Dont be lazy! Add a task.')
+    }
+})
 
 
 var control = document.getElementById('control')
@@ -61,10 +107,9 @@ const iniciar = () => {
     } else {
 
         for (const [k, tarea] of tienda.getState().lista.entries()) {
-            lista.innerHTML += `<li class="list-group-item align-middle">
-        ${tarea.tarea}
+            lista.innerHTML += `<li class="list-group-item align-middle">${tarea.tarea}
         <button type="button" onclick="tienda.dispatch(borrar(${k}))" class="btn btn-danger btn-sm float-right ml-1"><i class="fas fa-trash"></i></button>
-        <button type="button" class="btn btn-warning btn-sm float-right"><i class="fas fa-pen"></i></button>
+        <button type="button" class="btn btn-warning btn-sm float-right" onclick="mostrar(${false},'${tarea.tarea}',${k})"><i class="fas fa-pen"></i></button>
     </li>`
         }
     }
